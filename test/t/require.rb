24499c30a7cc0066b67c803d232ece3d41842ebe
Yuichiro MASUI <masui@masuidrive.jp>
2012-10-21 19:47:46 +0900

    Updated tests

--- test/t/require.rb
+++ test/t/require.rb
@@ -2,11 +2,11 @@ require 'require1'
 
 assert_equal 'REQUIRE_TEST_1', require_test_1
 assert_equal 123.45, REQUIRE_TEST_2
-#assert_raise(NoMethodError) do
-#  dummy = require_test_3
-#end
+assert_raise NoMethodError do
+  dummy = require_test_3
+end
 assert RequireTest4.is_a?(Class)
 
-#assert_raise(NoMethodError) do
-#  require 'require1_dummy'
-#end
+assert_raise ScriptError do
+  require 'require1_dummy'
+end
